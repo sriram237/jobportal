3c86b0613a5df58b28e120f23c07727b4afff69d
sriram237 <sriram23sm@gmail.com>
2026-02-12 21:36:58 +0530

    fix homepage route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,27 @@
-
 import { useState } from 'react'
 import './App.css'
 import Registerpage from './Registerpage.jsx'
 import LoginPage from './LoginPage.jsx'
 import JobListPage from './jobListPage.jsx'
 import ApplyJobPage from './ApplyJobPage.jsx'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
- 
 
   return (
     <BrowserRouter>
        <Routes>
+
+            {/* DEFAULT PAGE */}
+            <Route path="/" element={<Navigate to="/login" />} />
+
             <Route path='/register' element={<Registerpage/>}/>
             <Route path='/login'    element={<LoginPage/>}/>
             <Route path='/jobs' element={<JobListPage/>}/>
             <Route path='/apply/:jobId' element={<ApplyJobPage/>}/>
+
        </Routes>
-
     </BrowserRouter>
-
   )
 }
 
