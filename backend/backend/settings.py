a1438d2bf9f63773d5f952fb3f16b6a0aec23564
sriram237 <sriram23sm@gmail.com>
2026-02-13 21:26:42 +0530

    fix cors correctly

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -49,27 +49,30 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',  # MUST BE FIRST
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
+
+CORS_ALLOW_ALL_ORIGINS = False
+
 CORS_ALLOWED_ORIGINS = [
-    "https://jobportal-2-hl7t.onrender.com",
+    "http://localhost:5173",
+    "https://jobportal-2-hl7t.onrender.com"
 ]
 
 CORS_ALLOW_CREDENTIALS = True
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://jobportal-2-hl7t.onrender.com",
+    "http://localhost:5173",
+    "https://jobportal-2-hl7t.onrender.com"
 ]
-
-
 
 
 CSRF_TRUSTED_ORIGINS = [
